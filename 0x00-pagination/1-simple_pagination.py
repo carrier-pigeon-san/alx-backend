c85baedff8809f9f39a6f247cc3c88af0b1f893c
Sandy Obare <obaresandy@gmail.com>
2024-10-29 02:26:39 +0300

    1-simple_pagination.py update: index_range function syntax update

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -25,7 +25,7 @@ class Server:
 
         return self.__dataset
 
-    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
         """Return a tuple of size two containing a start index and an end index
         corresponding to the range of indexes to return in a list for those
         particular pagination parameters (page and page_size)."""
