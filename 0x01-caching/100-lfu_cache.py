f5c50871237908ca56688566f75beac48f41ee70
Sandy Obare <obaresandy@gmail.com>
2024-11-02 18:59:54 +0300

    100-lfu_cache.py update: a whole lot of changes

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,42 +1,48 @@
 #!/usr/bin/env python3
-"""LFUCache module
-"""
+""" LFUCache module """
 from collections import OrderedDict
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """A caching system using Least Frequently Used cache replacement algorithm
+    """ A caching system based on Least Frequently Used cache replacement
+    algorithm.
     """
     def __init__(self):
-        """LFUCache class constructor"""
+        """ Initialize class instance. """
         super().__init__()
         self.frequency = OrderedDict()
 
     def put(self, key, item):
-        """Adds an item to the cache using LFU cache replacement algorithm
-        """
-        if key and item:
-            self.cache_data.update({key: item})
-            self.frequency.update({key: 0})
-            if len(self.cache_data) > self.MAX_ITEMS:
-                for i, (k, v) in enumerate(self.frequency.items()):
-                    if i == 0:
-                        least = k
-                        least_v = v
-                        continue
-                    if v < least_v:
-                        least = k
-                        least_v = v
-                self.frequency.pop(least)
-                self.cache_data.pop(least)
-                print("DISCARD: {}".format(least))
+        """ Add an item in the cache with LFU algorithm. """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.frequency[key] += 1
+        else:
+            self.frequency[key] = 0
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > self.MAX_ITEMS:
+            for i, (k, v) in enumerate(self.frequency.items()):
+                if i == 0:
+                    lfu = k
+                    lfu_v = v
+                    continue
+                if i == len(self.frequency) - 1:
+                    break
+                if v < lfu_v:
+                    lfu = k
+                    lfu_v = v
+            del self.cache_data[lfu]
+            del self.frequency[lfu]
+            print("DISCARD: {}".format(lfu))
 
     def get(self, key):
-        """Get an item from the cache by key
-        """
-        if key and key in self.cache_data:
-            self.frequency[key] += 1
-            self.frequency.move_to_end(key)
-            return self.cache_data.get(key)
-        return None
+        """ Get an item by key using LFU cache replacement algorithm """
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.frequency[key] += 1
+        return self.cache_data[key]
