f1f4aa7ab4a4af356bb9308d9d3882c8c0079e25
Sandy Obare <obaresandy@gmail.com>
2024-11-05 13:57:05 +0300

    1-app.py initial commit

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Basic Flask app, with a single route and an index.html template """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+# Flask app instantiation
+app = Flask(__name__)
+
+
+# Config class for the app
+class Config:
+    """ Config class for the app """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# app configuration using the Config class
+app.config.from_object(Config)
+
+# Babel object instantiation
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route() -> str:
+    """ Returns a simple html page"""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
