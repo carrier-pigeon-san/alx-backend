6c27eb3ef03189724ac60a438985c15deba814ed
Sandy Obare <obaresandy@gmail.com>
2024-11-06 03:28:34 +0300

    4-app.py update: changed rendered template

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -34,7 +34,7 @@ def get_locale() -> str:
 @app.route('/', strict_slashes=False)
 def hello_route() -> str:
     """ Returns a simple html page"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
